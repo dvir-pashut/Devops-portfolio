95a81ca1d8c5df22ba05c38f64dff351e3261c53
dvir.p1999 <dvir.p1999@gmail.com>
2023-01-26 14:46:34 +0000

    added some pages

--- app.py
+++ app.py
@@ -1,32 +1,53 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template, request, redirect, url_for
-import os
 import re
+from pymongo import MongoClient
+
+
 
 app = Flask(__name__)
 
+client = MongoClient('172.17.0.2', 27017)
+
+db = client.flask_db
+books = db.books
+
 # post requestt
-@app.post("/triger")
-def staff_are_pushed():
-    content = str(request.get_json(silent=True)) #add the jason content to a string we can run tests on 
-    to_check = str(re.search("refs/heads/main", str(content))) #find the branch of the push
-    if to_check != "None": # if barnch = main send emails and run tests
-        print("merge to main!!!!!.... \n starting testings")  
-        os.system("bash phase1.sh ")
-        #########   figure out the place for us to send
-    return "push recived ... mails will be sent when merged with main"
+@app.post("/insert-book")
+def books_are_pushed():
+    content = request.form
+    book_name = request.form["book-name"]
+    Author_name = request.form["Author-name"]
+    email = request.form["email"]
+    rating = request.form["rating"]
+    summery = request.form["summery"]
+    books.insert_one({'book_name': book_name, 'Author_name' : Author_name, 'rating' : rating, 'summery' : summery, 'email' : email })
     
+    #print(content)
+    return content
 
 # home page(not required but nice to have)
 @app.get("/")
 def home_page():
     return render_template("index.html")
 
+@app.get("/contact.html")
+def contacts():
+    return render_template("contact.html")
+
+@app.get("/insert-book")
+def enter_books():
+    return render_template("enter-book.html")
+
+@app.get("/avliable-books")
+def get_books():
+    all_books = books.find()
+    return render_template("get-books.html", books=all_books)
 
 # helth checks(return ok... daaaaa)
 @app.get("/health")
 def health_check():
-    return "OK"
+    return "up"
 
 
 # monitor on health checks
