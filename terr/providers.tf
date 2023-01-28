6ee2fa19fc2a3cd040efdc4ebf4595dea889eab7
dvir pashut <pashutdvir@gmail.com>
2023-01-28 17:14:37 +0200

    wdbaskd

--- terr/providers.tf
+++ terr/providers.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.50.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.3.0"
+
+    }
+  }
+  backend "gcs" {
+    bucket  = "tf-state-dvir"
+    prefix  = "terraform/state"
+  }
+}
+
 provider "google" {
   project = "task-1"
   region  = "us-central1"
