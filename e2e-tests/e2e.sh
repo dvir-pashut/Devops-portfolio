7661fb659c019070debd700d9216d979082a7ad3
dvir pashut <pashutdvir@gmail.com>
2023-01-28 16:57:17 +0200

    added tf

--- e2e-tests/e2e.sh
+++ e2e-tests/e2e.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#sleep 10
+sleep 10
 
 #testing the main funcuins 
 curl -I localhost
---
+++ terr/providers.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  project = "task-1"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
