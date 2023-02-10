7e951f46776b33702f0c78d8699de57a9a974c75
Dvir Pashut <pashutdvir@gmail.com>
2023-02-10 17:31:35 +0200

    fixing the ci

--- e2e-tests/e2e.sh
+++ e2e-tests/e2e.sh
@@ -9,7 +9,8 @@ curl -I localhost/contact
 curl -o /dev/null -w "%{http_code}"  curl -X POST -d "name=some_name&email=example%40email.com&message=importantstuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuf" http://localhost/contact 
 
 for i in {1..100}; do
-  curl -o /dev/null -w "%{http_code}" curl -X POST -d "book-name=Book$i&Author-name=Author$i&summery=A+novel+about+the+decadence+and+excess+of+the+Roaring+Twenties&rating=$((i))&email=example%40email.com&submit=" http://localhost/insert-book
+  curl -X POST -d "book-name=Book$i&Author-name=Author$i&summery=A+novel+about+the+decadence+and+excess+of+the+Roaring+Twenties&rating=$((i))&email=example%40email.com&submit=" http://localhost/insert-book
+  docker ps
 done
 
 
